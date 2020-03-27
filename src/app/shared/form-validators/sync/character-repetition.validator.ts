8a9eccafe20e4567306af0c354d950d2fb0e4c7a
Ayrton DE ABREU MIRANDA <asadams89@gmail.com>
2020-03-27 16:08:39 +0100

    Modification du validateur empêchant la répétition de caractère -> conversion de la chaine en minuscule avant le contrôle

--- src/app/shared/form-validators/sync/character-repetition.validator.ts
+++ src/app/shared/form-validators/sync/character-repetition.validator.ts
@@ -3,16 +3,17 @@ import {AbstractControl, FormGroup, ValidatorFn} from '@angular/forms';
 export function CharacterRepetition(nbrAuthorized: number) {
   return (control: AbstractControl) => {
     if (control && control.value && control.value.length > 2 && nbrAuthorized > 2 && control.value.length >= nbrAuthorized) {
+      const value = String(control.value).toLowerCase();
 
       // On passe de caractère en caractère
-      const c = control.value.length;
+      const c = value.length;
       for (let i = 0; i < c; i++) {
 
         // Condition pour éviter les erreurs et éviter d'itéré sur une valeur non existante
         if ( c >= i + nbrAuthorized ) {
 
           // On récupère un chaine contenant le nombre de caractère autorisé à partir de la position i
-          const extraction = control.value.toString().substring(i, i + nbrAuthorized);
+          const extraction = value.substring(i, i + nbrAuthorized);
 
           // On test l'extraction pour savoir si tous les caractère la composant sont identiques au 1er
           // Si c'est différent
