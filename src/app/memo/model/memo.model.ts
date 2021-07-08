843ed7116b9172929af9ab17870a34c12605ab9c
Ayrton DE ABREU MIRANDA <asadams89@gmail.com>
2021-07-08 10:43:57 +0200

    Modification du model 'memo.model' -> en découle une modification dans 'memo.component.html'

--- src/app/memo/model/memo.model.ts
+++ src/app/memo/model/memo.model.ts
@@ -1,8 +1,10 @@
 export class MemoModel {
     constructor(public id: bigint | number,
-                public creationDate: Date,
-                public reminderDate: Date,
+                public lastModif: Date,
                 public title: string,
-                public content: string) {
+                public content: string,
+                public reminderByDate: {id: bigint | number, reminderDate: Date},
+                public reminderByDay: {id: bigint | number, monday: boolean, tuesday: boolean,
+                    wednesday: boolean, thursday: boolean, saturday: boolean, sunday: boolean}) {
     }
 }
